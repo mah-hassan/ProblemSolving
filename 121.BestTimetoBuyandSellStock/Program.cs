c0783374a5b3abc99acb58fef2bbfc0ba2fb5330
Mahmoud Hassan <113454774+mah-hassan@users.noreply.github.com>
2024-09-15 02:37:13 +0300

    121.BestTimetoBuyandSellStock

---
+++ 121.BestTimetoBuyandSellStock/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] stockPrices = [7, 1, 5, 3, 6, 4];
+
+var profit = new Solution().MaxProfit(stockPrices);
+
+Console.WriteLine(profit); // Output: 5
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int buy = prices[0], profit = 0;
+        for (int i = 1; i < prices.Length; i++)
+        {
+            if (prices[i] < buy)
+                buy = prices[i];
+            else 
+                profit = Math.Max(profit, prices[i] - buy);
+        }
+        return profit;
+    }
+}
