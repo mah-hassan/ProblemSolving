fb5011bc0fe4f57f4ce0f8c73e67dc477262e681
Mahmoud Hassan <113454774+mah-hassan@users.noreply.github.com>
2024-09-15 03:45:07 +0300

    1991. Find the Middle Index in Array

---
+++ 1991.FindtheMiddleIndexinArray/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int[] nums = [2, 3, -1, 8, 4];
+var middle = new Solution().FindMiddleIndex(nums);
+Console.WriteLine(middle);
+public class Solution
+{
+    public int FindMiddleIndex(int[] nums)
+    {
+        int totalSum = nums.Sum(), leftSum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (leftSum == totalSum - leftSum - nums[i])
+            {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
