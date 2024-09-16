691d002409dbb89acf9bb06e165f02370abe0a82
Mahmoud Hassan <113454774+mah-hassan@users.noreply.github.com>
2024-09-16 03:32:41 +0300

    141. Linked List Cycle

---
+++ 141.LinkedListCycle/Program.cs
@@ -0,0 +1,27 @@
+﻿
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+ 
+public class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
