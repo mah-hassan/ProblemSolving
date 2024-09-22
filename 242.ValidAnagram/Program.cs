6592837db98e772560adf35df643202c8bc3cf3a
Mahmoud Hassan <113454774+mah-hassan@users.noreply.github.com>
2024-09-22 16:27:13 +0300

    242. Valid Anagram

---
+++ 242.ValidAnagram/Program.cs
@@ -0,0 +1,30 @@
+﻿string s = "a", t = "ab";
+
+Console.WriteLine(new Solution().IsAnagram(s, t)); // Output: false
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        Dictionary<char, int> sLettersFrequancy = new();
+        foreach (var c in s)
+        {
+            if (sLettersFrequancy.ContainsKey(c))
+            {
+                sLettersFrequancy[c]++;
+            }
+            else
+            {
+                sLettersFrequancy[c] = 1;
+            }
+        }
+        foreach (var c in t)
+        {
+            if(!sLettersFrequancy.ContainsKey(c))
+                return false;
+            else if(sLettersFrequancy[c] == 1)
+                sLettersFrequancy.Remove(c);                
+            else
+                sLettersFrequancy[c]--;
+        }
+        return sLettersFrequancy.Count == 0;
+
+    }
+}
