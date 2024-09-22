e62155fbc3e849c40729f9b8cb03bcc5b03ae8fa
Mahmoud Hassan <113454774+mah-hassan@users.noreply.github.com>
2024-09-22 04:07:37 +0300

    973. K Closest Points to Origin

---
+++ 973.KClosestPointsToOrigin/Program.cs
@@ -0,0 +1,26 @@
+﻿int[][] points = [[3,3],[5,-1],[-2,4]];
+var k = 2;
+var result = new Solution().KClosest(points, k);
+foreach (var point in result)
+{
+    Console.WriteLine($"(X:{point[0]}, Y:{point[1]})");  // Output: (3,3), (-2,4)
+}
+public class Solution
+{
+    public int[][] KClosest(int[][] points, int k) 
+    {
+        PriorityQueue<int[], double> priorityQueue = new();
+        for (int i = 0; i < points.Length; i++)
+        {
+            var point = points[i];
+            var distance = Math.Sqrt(Math.Pow((point[0] - 0), 2) + Math.Pow((point[1] - 0),2));
+            priorityQueue.Enqueue(point, distance);
+        }
+        int[][] result = new int[k][];
+        for(int i = 0; i < k; i++)
+        {
+            result[i] = priorityQueue.Dequeue();
+        }
+        return result;
+    }
+}
