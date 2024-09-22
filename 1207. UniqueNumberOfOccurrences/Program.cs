d1b827c87d83fb2565ce9b69dd0ee63216d373f6
Mahmoud Hassan <113454774+mah-hassan@users.noreply.github.com>
2024-09-23 01:58:53 +0300

    1207. Unique Number of Occurrences

---
+++ 1207. UniqueNumberOfOccurrences/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] arr = [1,2];
+
+Solution solution = new Solution();
+
+bool uniqueOccurrences = solution.UniqueOccurrences(arr);
+
+Console.WriteLine(uniqueOccurrences); 
+
+public class Solution {
+    public bool UniqueOccurrences(int[] arr) 
+    {
+        Dictionary<int, int> frequencies = new();
+         
+        foreach (var num in arr)
+        {
+            if (frequencies.ContainsKey(num))
+                frequencies[num]++;
+            else
+                frequencies[num] = 1;
+        }
+
+        
+        HashSet<int> uniqueOccurrences = new();
+        
+        foreach (var freq in frequencies.Values)
+        {
+            if (!uniqueOccurrences.Add(freq)) // If freq already exists, return false
+                return false;
+        }
+
+        return true;
+    }
+}
