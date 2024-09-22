3bee78cdf93c9ccde2b65682029a493c4501243d
Mahmoud Hassan <113454774+mah-hassan@users.noreply.github.com>
2024-09-23 02:09:22 +0300

    217. Contains Duplicate

---
+++ 217.ContainsDuplicate/Program.cs
@@ -0,0 +1,20 @@
+﻿int[]  nums = [1,2,3,4];
+
+var solution = new Solution();
+
+var result = solution.ContainsDuplicate(nums);
+
+Console.WriteLine($"The array contains duplicate numbers: {result}");
+
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+
+        HashSet<int> uniqeNums = new HashSet<int>();
+
+        foreach (var num in nums)
+            if(!uniqeNums.Add(num))
+                return true;
+
+        return false;
+    }
+}
