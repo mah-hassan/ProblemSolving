29ce4911ace45344b37b8b6d0b4918c5d70b8b58
Mahmoud Hassan <113454774+mah-hassan@users.noreply.github.com>
2024-09-27 00:07:24 +0300

    230. Kth Smallest Element in a BST

---
+++ 230.KthSmallestElementInaBST/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+public class TreeNode {
+    public int val;
+    public TreeNode? left;
+    public TreeNode? right;
+    public TreeNode(int val=0, TreeNode? left = null, TreeNode? right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class Solution {
+    public int KthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> nodes = new();
+        int kth = 0; // do InOrder DFS traversal until kth is equal to k which is the target
+        var current = root;
+        while(nodes.Count > 0 || current is not null)
+        {
+            while(current is not null)
+            {
+                nodes.Push(current);
+                current = current.left;
+            }
+            current = nodes.Pop();
+            kth++;
+            if(kth == k)
+                return current.val; 
+            current = current.right;
+        }
+        return -1;
+    }
+}
