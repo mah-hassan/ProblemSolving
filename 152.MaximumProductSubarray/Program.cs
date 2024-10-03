874064ae64e99c1cec9424ec91e446ef2cebf8bc
Mahmoud Hassan <113454774+mah-hassan@users.noreply.github.com>
2024-10-04 00:40:16 +0300

    152. Maximum Product Subarray

---
+++ 152.MaximumProductSubarray/Program.cs
@@ -0,0 +1,24 @@
+﻿var solution = new Solution();
+
+int[] nums =  [2, 3, -2, 4];
+
+int result = solution.MaxProduct(nums);
+
+Console.WriteLine($"The maximum product of a contiguous subarray is: {result}");
+
+public class Solution {
+    public int MaxProduct(int[] nums) {
+        int currMaxProduct = nums[0];
+        int currMinProduct = nums[0];
+        int result = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            int temp = currMaxProduct;
+            currMaxProduct = Math.Max(nums[i], Math.Max(nums[i] * currMaxProduct, nums[i] * currMinProduct));
+            currMinProduct = Math.Min(nums[i], Math.Min(nums[i] * temp, nums[i] * currMinProduct));
+            result = Math.Max(result, currMaxProduct);
+        }
+
+        return result;
+    }
+}
