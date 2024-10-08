1f9bfc8c1ce4629fe58f53679e5aa9d7d5318fbe
Mahmoud Hassan <113454774+mah-hassan@users.noreply.github.com>
2024-10-09 00:19:37 +0300

    3. Longest Substring Without Repeating Characters

---
+++ 3.LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,27 @@
+﻿
+string s = "pwwkew";
+
+Solution solution = new Solution();
+
+int maxLength = solution.LengthOfLongestSubstring(s);
+
+Console.WriteLine(maxLength);  // Output: 3
+
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        int maxLength = 0;
+        int left = 0;
+        HashSet<char> letters = new();
+        for(int i = 0; i < s.Length; i++) 
+        {
+            while(letters.Contains(s[i]))
+            {
+                letters.Remove(s[left]);
+                left++;  
+            }
+            letters.Add(s[i]);
+            maxLength = Math.Max(maxLength, letters.Count);
+        }
+        return maxLength;
+    }
+}
